1cb99a644524828148fb66bb0e68b26293b9b4f8
wisnieskii <106274579+wisnieskii@users.noreply.github.com>
2022-08-08 13:43:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './App.css'
+import tmdb from "./tmdb"
+import MoviesRow from './components/MoviesRow'
+import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header'
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(
+    () => {
+      const loadAll = async () => {
+        let list = await tmdb.getHomeList();
+        setMovieList(list);
+
+        let originals = list.filter(i => i.slug === 'originals')
+        let randomChosem = Math.floor(Math.random() * (originals[0].items.results?.length - 1))
+        let chosen = originals[0].items.results[randomChosem]
+        let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv')
+
+        setFeaturedData(chosenInfo)
+      }
+      loadAll();
+    }, []);
+
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="page">
+      <Header black={blackHeader} />
 
-export default App;
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MoviesRow key={key} title={item.title} items={item.items} />
+        ))}
+
+      </section>
+      <footer>
+        Feito com <span role='img' aria-label='coração'>💗</span> Amanda Wisnieski <br />
+        Direitos de imagem para Netflix<br />
+        Dados pegos do site Themoviedb.org
+
+      </footer>
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://rchandru.com/images/portfolio/modals/m-loading.gif" />
+        </div>}
+
+
+    </div>
+  )
+};
